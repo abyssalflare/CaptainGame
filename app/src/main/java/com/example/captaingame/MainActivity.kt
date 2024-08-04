6f93dabc678f21e1175f939b540b143d581f29a0
Jeryl Goh <jerylgoh2003@gmail.com>
2024-08-04 15:30:17 +0800

    Captain Game Base Code
    used remember, mutablestate, to create simply captain treasure finding game

--- app/src/main/java/com/example/captaingame/MainActivity.kt
+++ app/src/main/java/com/example/captaingame/MainActivity.kt
@@ -3,14 +3,22 @@ package com.example.captaingame
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.captaingame.ui.theme.CaptainGameTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +30,83 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    captainGame();
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun captainGame()
+    {
+        val treasuresFound = remember { mutableStateOf(0) }
+        val direction = remember { mutableStateOf("North") }
+        val stormOrTreasure = remember { mutableStateOf("")}
+        
+        Column{
+            Text(text = "Treasures Found: ${treasuresFound.value}")
+            Spacer(modifier = Modifier.height(16.dp));
+            Text(text = "Current Direction: ${direction.value}")
+            Spacer(modifier = Modifier.height(16.dp));
+            Button(onClick = {
+                direction.value = "East";
+                if(Random.nextBoolean())
+                {
+                    treasuresFound.value++;
+                    stormOrTreasure.value = "Treasure!!!";
+                }
+                else
+                {
+                    stormOrTreasure.value = "a storm..."
+                }
+            }) {
+                Text(text = "Sail East")
+            }
+            Button(onClick = {
+                direction.value = "West";
+                if(Random.nextBoolean())
+                {
+                    treasuresFound.value++;
+                    stormOrTreasure.value = "Treasure!!!";
+                }
+                else
+                {
+                    stormOrTreasure.value = "a storm..."
+                }
+            }) {
+                Text(text = "Sail West")
+            }
+            Button(onClick = {
+                direction.value = "North";
+                if(Random.nextBoolean())
+                {
+                    treasuresFound.value++;
+                    stormOrTreasure.value = "Treasure!!!";
+                }
+                else
+                {
+                    stormOrTreasure.value = "a storm..."
+                }
+            }) {
+                Text(text = "Sail North")
+            }
+            Button(onClick = {
+                direction.value = "South";
+                if(Random.nextBoolean())
+                {
+                    treasuresFound.value++;
+                    stormOrTreasure.value = "Treasure!!!";
+                }
+                else
+                {
+                    stormOrTreasure.value = "a storm..."
+                }
+            }) {
+                Text(text = "Sail South")
+            }
+            Spacer(modifier = Modifier.height(16.dp));
+            Text(text = "You encountered: ${stormOrTreasure.value}");
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CaptainGameTheme {
-        Greeting("Android")
+        }
     }
 }
